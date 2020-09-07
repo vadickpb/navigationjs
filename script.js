6c4240b20f1940b38b8dffbd47baa4e20ecd7200
vadickpb <vadick1@gmail.com>
2020-09-07 17:24:27 -0500

    finalizando el proyecto con el codigo optimizado

--- script.js
+++ script.js
@@ -1,13 +1,45 @@
+const overlay = document.querySelector("#overlay");
 const menuBars = document.querySelector('#menu-bars');
-const bar1 = document.querySelector('#bar1');   
-const bar2 = document.querySelector('#bar2');   
-const bar3 = document.querySelector('#bar3');   
+const nav1 = document.querySelector('#nav-1');   
+const nav2 = document.querySelector('#nav-2');   
+const nav3 = document.querySelector('#nav-3');   
+const nav4 = document.querySelector('#nav-4');   
+const nav5 = document.querySelector('#nav-5');  
+const navItems = [nav1, nav2, nav3, nav4, nav5];
+
+function navAnimation(direction1, direction2){
+    navItems.forEach((item, i) => {
+        item.classList.replace(`slide-${direction1}-${i+1}`,`slide-${direction2}-${i+1}`)
+    })
+}
+
 
 function toggleNav(){
+    //toggle menu bar open closed
     menuBars.classList.toggle('change');
+
+    //toggle: Menu active
+    overlay.classList.toggle('overlay-active');
+    if (overlay.classList.contains('overlay-active')) {
+        //Animate in - Overlay
+        overlay.classList.replace('overlay-slide-left', 'overlay-slide-right');
+
+        //Animate in item
+        navAnimation('out' , 'in');
+    
+
+    } else {
+        //Animate out - Overlay
+        overlay.classList.replace('overlay-slide-right', 'overlay-slide-left');
+
+        //animate out items
+        navAnimation('in' , 'out');
+        
+    }
 }
 
 menuBars.addEventListener('click', toggleNav);
-bar1.addEventListener('click', toggleNav);
-bar2.addEventListener('click', toggleNav);
-bar3.addEventListener('click', toggleNav);
+navItems.forEach( item => {
+    item.addEventListener('click', toggleNav);
+})
+
